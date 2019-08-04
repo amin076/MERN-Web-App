4ea721589ab422b5fcfb5b63bae0a8a352884b15
amin <amin.nazari076@gmail.com>
2019-08-04 17:19:16 +1000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Greet from './component/Greet';
+import Welcome from './component/Welcome';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Greet/>
+      
+      <Welcome/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ Greet.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+
+function Greet(){
+    return <h1> hi there</h1>
+}
+
+export default Greet;
